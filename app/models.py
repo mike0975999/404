42457aa8c16e6dfa2ac286b1133ce19aba7b455f
Mike <mikefvargas24@gmail.com>
2026-04-19 18:28:08 +0800

    add chapter dependencies

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from sqlmodel import SQLModel, Field
 
 class Book(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    book_id: int
+    chapter_number: int
     title: str
     author: str
     course: str
