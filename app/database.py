7fdc9d7b46f980b3dd1a813ee82ad6b958375041
Mike <mikefvargas24@gmail.com>
2026-04-19 17:53:19 +0800

    switch to sqlite for render

--- app/database.py
+++ app/database.py
@@ -4,10 +4,12 @@ from sqlmodel import SQLModel, create_engine
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./test.db"
 print("DATABASE_URL =", DATABASE_URL)
 
-engine = create_engine(DATABASE_URL, echo=True)
-
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
